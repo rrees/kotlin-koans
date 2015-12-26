ac630b7eb1579d33bd54ddcb8ca5c51635c2edf7
Robert Rees <robert.rees@guardian.co.uk>
2015-12-26 20:44:05 +0000

    Completes the weirdly specific SAM example

--- src/i_introduction/_11_SAM_Conversions/SAMConversions.kt
+++ src/i_introduction/_11_SAM_Conversions/SAMConversions.kt
@@ -16,6 +16,6 @@ fun todoTask11(): Nothing = TODO(
 
 fun task11(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> todoTask11() })
+    Collections.sort(arrayList, { x, y -> if(x > y) -1 else if(x < y) 1 else 0 })
     return arrayList
 }
