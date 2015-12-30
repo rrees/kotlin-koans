cbc45991aecc8ae31ba257d3ee33f5f0f98ed15c
Robert Rees <robert.rees@guardian.co.uk>
2015-12-30 21:33:34 +0000

    Completes the section on generics and serves as a timely reminder how difficult generic typed functions are to implement

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -20,12 +20,18 @@ fun task41(): Nothing = TODO(
         }
 )
 
+public fun <T, C : MutableCollection<in T>> Collection<T>.partitionTo(first: C, last: C, predicate: (T) -> Boolean): Pair<C, C> {
+    forEach {
+        if(predicate(it)) first.add(it)
+        else last.add(it)
+    }
+    return Pair(first, last)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
